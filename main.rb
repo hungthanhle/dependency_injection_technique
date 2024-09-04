cfa497eb0df09928031a90f34dd6740b4061a28d
hung <hunglt.ee@gmail.com>
2024-09-04 16:49:57 +0700

    dependency_injection_program_technique

--- main.rb
+++ main.rb
@@ -2,16 +2,14 @@ require_relative 'email_sender'
 require_relative 'services'
 
 def main
-  sender = EmailSender.new
+  email_service = MailChimp.new(attachment: attachment)
+
+  sender = EmailSender.new(email_service: email_service) # injection obj
 
   result = sender.send_email(
-    service: Service::MAILCHIMP,
     to_address: "arjan@arjancodes.com",
     subject: "Urgent meeting",
-    body: "Very secret email",
-    signature: "",
-    smtp_server: "",
-    attachment: ""
+    body: "Very secret email"
   )
 
   puts result
