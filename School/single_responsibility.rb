239948678d2b32dd8bdf0900fe4e99b332ddd81b
hung <hunglt.ee@gmail.com>
2024-09-12 22:47:51 +0700

    problem_S

---
+++ School/single_responsibility.rb
@@ -0,0 +1,10 @@
+# https://youtu.be/a0wDsf9YjSw?si=NS1SyB5EvsMwl7xl&t=944
+class OrderManager
+  def create_order
+    puts "Create order."
+  end
+
+  def send_email
+    puts "Send email."
+  end
+end
